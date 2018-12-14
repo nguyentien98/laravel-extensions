f42bbae1d5c92c60a59ec905415a2f5b90a28207
Nguyễn Xuân Quỳnh <nguyenxuanquynh2210vghy@gmail.com>
2018-12-14 13:04:10 +0700

    Configure phpcs fixer

--- tests/Support/PresenterTest.php
+++ tests/Support/PresenterTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Xuanquynh Laravel Extensions package.
+ *
+ * (c) Nguyễn Xuân Quỳnh
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace XuanQuynh\Laravel\Tests\Support;
 
 use Mockery as m;
